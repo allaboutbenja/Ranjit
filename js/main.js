c9e11e1368e443a70f724a371ca04b5f72a55b89
sebastian.torrealba <sebastian.torrealba@tgtgroup.cl>
2022-11-24 09:00:16 -0300

    sweetalert added

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ const toggleCarshop = () => {
     shadowCarshop$.classList.toggle('is-active')
 }
 
+
+
+const showMessage = (title,icon = 'success', timer = 1500,) => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon,
+        title
+      })
+}
